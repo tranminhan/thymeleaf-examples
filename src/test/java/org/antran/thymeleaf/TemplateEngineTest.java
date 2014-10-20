851bb9ea9cf2c3abbcf0c3ec659bc8b5dcb29b11
An Tran Minh <atran@atlassian.com>
2014-10-20 22:17:16 +0700

    test process content

--- src/test/java/org/antran/thymeleaf/TemplateEngineTest.java
+++ src/test/java/org/antran/thymeleaf/TemplateEngineTest.java
@@ -1,25 +1,16 @@
 package org.antran.thymeleaf;
 
-import static org.junit.Assert.*;
-
-import java.util.Collections;
-import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.dialect.IDialect;
-import org.thymeleaf.standard.StandardDialect;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.testing.templateengine.context.IProcessingContextBuilder;
-import org.thymeleaf.testing.templateengine.context.web.WebProcessingContextBuilder;
-import org.thymeleaf.testing.templateengine.report.ConsoleTestReporter;
-import org.thymeleaf.testing.templateengine.report.ITestReporter;
-import org.thymeleaf.testing.templateengine.resolver.ITestableResolver;
-import org.thymeleaf.testing.templateengine.standard.resolver.StandardTestableResolver;
-import org.thymeleaf.testing.templateengine.testable.ITest;
 
 public class TemplateEngineTest {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void shouldProcessTemplateWithoutException() {
@@ -31,7 +22,8 @@ public class TemplateEngineTest {
         templateResolver.setCharacterEncoding("UTF-8");
         templateEngine.setTemplateResolver(templateResolver);
 
-        templateEngine.process("home.html", contex);
+        final String result = templateEngine.process("home.html", contex);
+        logger.info("result: " + result);
     }
 
 }
